74824e3d81bcad965ea0196b2bbd23970c94f0b1
Ismael Medeiros <IsmaelM@lacunasoftware.com>
2017-03-31 20:28:06 -0300

    Fixed required for parent class;

--- lib/rest_pki.rb
+++ lib/rest_pki.rb
@@ -7,13 +7,13 @@ require_relative "rest_pki/authentication"
 require_relative "rest_pki/errors"
 require_relative "rest_pki/object"
 require_relative "rest_pki/pades_signature"
+require_relative "rest_pki/xml_signature_starter"
 require_relative "rest_pki/full_xml_signature_starter"
 require_relative "rest_pki/namespace_manager"
 require_relative "rest_pki/xml_element_signature_starter"
 require_relative "rest_pki/xml_id_resolution_table"
 require_relative "rest_pki/xml_insertion_options"
 require_relative "rest_pki/xml_signature_finisher"
-require_relative "rest_pki/xml_signature_starter"
 
 Dir[File.expand_path('../rest_pki/resources/*.rb', __FILE__)].map do |path|
   require path
