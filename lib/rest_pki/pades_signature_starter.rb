0e410434bda34f112e26ff3c4faf40c8d96362ac
Ismael Medeiros <140083162@aluno.unb.br>
2017-12-12 12:45:34 -0200

    Added new flag for revocation status unknown;

--- lib/rest_pki/pades_signature_starter.rb
+++ lib/rest_pki/pades_signature_starter.rb
@@ -83,7 +83,8 @@ module RestPki
                 pdfToSign: @pdf_content_base64,
                 signaturePolicyId: @signature_policy_id,
                 callbackArgument: @callback_argument,
-                visualRepresentation: @visual_representation
+                visualRepresentation: @visual_representation,
+                ignoreRevocationStatusUnknown: @ignore_revocation_status_unknown
             }
             unless @signer_certificate_base64.to_s.blank?
                 request['certificate'] = Base64.encode64(@signer_certificate_base64)
