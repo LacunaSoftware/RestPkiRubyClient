b4131a9541f59a9802b82ddea3e73efa39a35fab
EmilyS <emily.souza.rs@gmail.com>
2020-04-22 17:36:31 -0300

    Added pades-explorer without model

---
+++ lib/rest_pki/pades_signature_explorer.rb
@@ -0,0 +1,18 @@
+module RestPki
+    PDF_MIME_TYPE = 'application/pdf'
+    class PadesSignatureExplorer < SignatureExplorer
+        def initialize(restpki_client)
+            super(restpki_client)
+        end
+
+        def open()
+            if @signature_file_content.to_s.empty?
+                raise 'The signature file to be opened not set'
+            end
+
+            request = get_request(PDF_MIME_TYPE)
+            response = @restpki_client.post('Api/PadesSignatures/Open', request, 'pades_explorer_model')
+            response
+        end
+    end
+end
---
+++ lib/rest_pki/signature_explorer.rb
@@ -0,0 +1,48 @@
+require 'base64'
+
+module RestPki
+    class SignatureExplorer
+        attr_accessor :default_signature_policy_id, :security_context_id, :acceptable_explicit_policies, :validate
+        
+        def initialize(restpki_client)
+            @restpki_client = restpki_client
+            @signature_file_content = nil
+            @default_signature_policy_id = nil
+            @security_context_id = nil
+            @validate = true
+            @acceptable_explicit_policies = nil;
+        end
+
+        def set_signature_file_from_path(path)
+            file = File.open(path, 'rb')
+            @signature_file_content = file.read
+            file.close
+        end
+      
+        def set_signature_file_from_content_raw(content_raw)
+            @signature_file_content = content_raw
+        end
+        
+        def set_signature_file_from_content_base64(content_base64)
+            @signature_file_content =  Base64.decode64(content_base64)
+        end
+
+        protected
+        def get_request(mimeType)
+            request = {
+                validate: @validate,
+                securityContextId: @security_context_id,
+                acceptableExplicitPolicies: @acceptable_explicit_policies,
+                defaultSignaturePolicyId: @default_signature_policy_id
+            }
+
+            unless @signature_file_content.to_s.empty?
+                request['file'] = {
+                    content: @signature_file_content,
+                    mimeType: mimeType
+                }
+            end
+            request
+        end
+    end
+end
